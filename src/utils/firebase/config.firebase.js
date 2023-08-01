7ba4a030e249a44662215b215af7e7b0769dfbe2
nf-714 <nfuad714@gmail.com>
2023-08-01 09:08:24 +0600

    Security Fixing

--- src/utils/firebase/config.firebase.js
+++ src/utils/firebase/config.firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB5ZAuymmGaNQ1Fla7vX-HYtDxMVxMe5fU",
-  authDomain: "genx-commerce.firebaseapp.com",
-  projectId: "genx-commerce",
-  storageBucket: "genx-commerce.appspot.com",
-  messagingSenderId: "751466070402",
-  appId: "1:751466070402:web:9cb931f5ab24547a080f9b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
